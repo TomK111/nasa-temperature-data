46cacd7c212e6590a2f529d33fcc7d9e9c739c1d
Tomislav Kraljic <63562144+TomislavKraljic@users.noreply.github.com>
2020-07-25 17:31:56 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// Data from : https://data.giss.nasa.gov/gistemp/
+const xlabels = [];
+const ytemps = [];
+
+chartIt();
+getData();
+
+async function chartIt() {
+    await getData();
+    const ctx = document.getElementById('chart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xlabels,
+            datasets: [{
+                label: 'Combined Land-Surface Air and Sea-Surface Temperature C°',
+                data: ytemps,
+                fill: true,
+                backgroundColor: ['rgba(77, 5, 232, 1)'],
+                borderColor: [
+                    ['rgba(77, 5, 232, 1)'],
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [
+                    {
+                    ticks: {
+                        easing: 'linear',
+                        responsive: true,
+                        callback: function (value, index, values) {
+                            return value + '°';
+                        }
+                    }
+                }]
+            }
+        }
+    });
+
+    async function getData() {
+        const response = await fetch('ZonAnn.Ts+dSST.csv')
+        const data = await response.text();
+    
+
+        const table = data.split('\n').slice(1);
+        table.forEach(row => {
+            const columns = row.split(',');
+            const year = columns[0];
+            xlabels.push(year);
+            const temp = columns[1];
+            ytemps.push(parseFloat(temp)+14);
+            console.log(year, temp);
+        })
+    }
+}
